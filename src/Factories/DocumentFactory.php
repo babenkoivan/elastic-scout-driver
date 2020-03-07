52192f2ff09bc8b96e1cff2fb3f3326f0f967261
ivanbabenko <babenko.i.a@gmail.com>
2020-03-07 13:40:37 +0100

    Stop using protected method

--- src/Factories/DocumentFactory.php
+++ src/Factories/DocumentFactory.php
@@ -6,6 +6,7 @@ namespace ElasticScoutDriver\Factories;
 use ElasticAdapter\Documents\Document;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Collection as BaseCollection;
 use Laravel\Scout\Searchable;
 
@@ -15,7 +16,10 @@ final class DocumentFactory implements DocumentFactoryInterface
     {
         return $models->map(function (Model $model) {
             /** @var Searchable $model */
-            if ($model::usesSoftDelete() && config('scout.soft_delete', false)) {
+            if (
+                in_array(SoftDeletes::class, class_uses_recursive(get_class($model))) &&
+                config('scout.soft_delete', false)
+            ) {
                 $model->pushSoftDeleteMetadata();
             }
 
